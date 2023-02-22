2f0bceceff7c22c0dc617589446a82d3d51ff175
tororo_1066 <panshoku4@gmail.com>
2023-02-22 13:58:45 +0900

    完成

--- src/main/java/tororo1066/man10rta/RTATask.kt
+++ src/main/java/tororo1066/man10rta/RTATask.kt
@@ -1,7 +1,6 @@
 package tororo1066.man10rta
 
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.TranslatableComponent
 import org.bukkit.Bukkit
 import org.bukkit.Server
 import org.bukkit.event.player.PlayerAdvancementDoneEvent
@@ -9,17 +8,12 @@ import org.bukkit.event.player.PlayerKickEvent
 import tororo1066.tororopluginapi.SJavaPlugin
 import tororo1066.tororopluginapi.SStr
 import tororo1066.tororopluginapi.sEvent.SEvent
-import java.text.SimpleDateFormat
 import java.util.*
 
 class RTATask: Thread() {
 
-    override fun start() {
-        val dateFormat = SimpleDateFormat("HH:mm:ss.SSS")
+    override fun run() {
         val startDate = Date()
-        sleep(10)
-        val debugDate = Date()
-        Bukkit.broadcastMessage((debugDate.time-startDate.time).toString())
         var lock = true
         var finishedPlayer: String? = null
         var finishedAdvancement: Component? = null
@@ -28,20 +22,20 @@ class RTATask: Thread() {
             val data = Man10RTA.data[e.advancement.key.key]?:return@register
             if (!data.isEnabled)return@register
             val nowDate = Date()
+            val time = between(nowDate,startDate)
             if (data.count){
-                e.player.sendMessage(Component.text(Man10RTA.prefix.toString()).append(data.displayName).append(Component.text("§fを達成しました！ §d§l達成時間: ${dateFormat.format(Date(nowDate.time-startDate.time))}")))
+                e.player.sendMessage(Component.text(Man10RTA.prefix.toString()).append(data.displayName).append(Component.text("§fを達成しました！ §d§l達成時間: $time")))
                 if (data.isWriteAll){
                     val list = SJavaPlugin.plugin.config.getConfigurationSection(data.name)?.getStringList("clearTime")?:return@register
-                    list.add("${e.player.uniqueId}:${e.player.name}:${dateFormat.format(Date(nowDate.time-startDate.time))}")
+                    list.add("${e.player.uniqueId}:${e.player.name}:${between(Date(),startDate)}")
                     SJavaPlugin.plugin.config.getConfigurationSection(data.name)?.set("clearTime",list)
                     return@register
                 }
             }
             data.count = true
-            SJavaPlugin.plugin.config.getConfigurationSection(data.name)?.set("clearTime","${e.player.uniqueId}:${e.player.name}:${dateFormat.format(Date(nowDate.time-startDate.time))}")
+            SJavaPlugin.plugin.config.getConfigurationSection(data.name)?.set("clearTime","${e.player.uniqueId}:${e.player.name}:${time}")
             SJavaPlugin.plugin.saveConfig()
-            Bukkit.broadcast(Component.text(Man10RTA.prefix.toString() + "§e§l${e.player.name}§fが§r").append(data.displayName).append(Component.text("§fを達成しました！ §d§l達成時間: ${dateFormat.format(Date(nowDate.time-startDate.time))}")),Server.BROADCAST_CHANNEL_USERS)
-            (Man10RTA.prefix + SStr("&e&l${e.player.name}&fが&a${data.displayName}&fを達成しました！ &d&l達成時間: ${dateFormat.format(Date(nowDate.time-startDate.time))}")).broadcast()
+            Bukkit.broadcast(Component.text(Man10RTA.prefix.toString() + "§e§l${e.player.name}§fが§r").append(data.displayName).append(Component.text("§fを達成しました！ §d§l達成時間: $time")),Server.BROADCAST_CHANNEL_USERS)
 
             if (data.isKillServer){
                 finishedPlayer = e.player.name
@@ -52,13 +46,24 @@ class RTATask: Thread() {
 
         while (lock) {
             Bukkit.getOnlinePlayers().forEach {
-                it.sendActionBar(SStr("&a&l経過時間: &d&l${dateFormat.format(Date(Date().time - startDate.time))}").toPaperComponent())
+                it.sendActionBar(SStr("&a&l経過時間: &d&l${between(Date(),startDate)}").toPaperComponent())
             }
-            sleep(1000)
+            sleep(50)
         }
 
-        Bukkit.getOnlinePlayers().forEach {
-            it.kick(finishedAdvancement?.let { it1 -> Component.text("${finishedPlayer}が").append(it1).append(Component.text("を達成しました")) },PlayerKickEvent.Cause.TIMEOUT)
-        }
+        Bukkit.getScheduler().runTask(SJavaPlugin.plugin, Runnable {
+            Bukkit.getOnlinePlayers().forEach {
+                it.kick(finishedAdvancement?.let { it1 -> Component.text("${finishedPlayer}が").append(it1).append(Component.text("を達成しました")) },PlayerKickEvent.Cause.TIMEOUT)
+            }
+        })
+    }
+
+    private fun between(firstDate: Date,minusDate: Date): String {
+        val time = firstDate.time - minusDate.time
+        val hour = time / 3600000
+        val minute = (time - hour * 3600000) / 6000
+        val second = (time - hour * 36000000 - minute * 6000) / 1000
+        val millisecond = time - hour * 36000000 - minute * 6000 - second * 1000
+        return "${hour}:${minute}:${second}.${millisecond}"
     }
 }
